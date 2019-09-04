d5901ea52db0c0d5c200bd86f079af095a06627f
Camilo Valenzuela <cacke91@gmail.com>
2019-09-04 15:07:52 -0400

    Removed footer

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
   <div id="app">
     <Navbar/>
     <router-view/>
-    <Footer/>
+    <!-- <Footer/> -->
   </div>
 </template>
 
 
 <script>
 import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+// import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    Footer
+    // Footer
   },
   data () {
     return {
