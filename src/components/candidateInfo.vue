fffd741b32990914c1f7800613c4d1cdafd3ae63
Camilo Valenzuela <cacke91@gmail.com>
2019-09-12 12:56:46 -0300

    Added stamps and rising time

--- src/components/candidateInfo.vue
+++ src/components/candidateInfo.vue
@@ -31,31 +31,29 @@
                <span class="font-weight-black">Mag:</span>
                 {{data["Discovery Mag"]}}  (<span class="font-weight-black">Band:</span> {{data["Discovery Mag Filter"].replace("-ZTF","")}})
              </v-flex>
-             <v-flex xs8>
+             <v-flex xs7>
                <span class="font-weight-black">Host:</span>
                 {{data["Host Name"]?data["Host Name"]:"-"}}
              </v-flex>
-             <v-flex xs4>
+             <v-flex xs5>
                <span class="font-weight-black">H.Redshift:</span>
                 {{data["Host Redshift"]?data["Host Redshift"].toFixed(3):"-"}}
              </v-flex>
-             <v-divider></v-divider>
+             <!-- <v-divider></v-divider> -->
              <template v-if="data.Name.includes('SN')">
-               <v-flex xs6>
+               <v-flex xs7>
                  <span class="font-weight-black">Type:</span>
                   {{data["Obj. Type"]}}
                </v-flex>
-               <v-flex xs6>
+               <v-flex xs5>
                  <span class="font-weight-black">Redshift:</span>
                   {{data["Redshift"].toFixed(3)}}
                </v-flex>
-               <v-divider></v-divider>
+               <!-- <v-divider></v-divider> -->
              </template>
-             <v-flex xs6>
+             <v-flex xs12>
                <span class="font-weight-black">Ra:</span>
                {{data.RA}}
-             </v-flex>
-             <v-flex xs6>
                <span class="font-weight-black">Dec:</span>
                {{data.DEC}}
              </v-flex>
@@ -69,7 +67,40 @@
                </div>
                <div v-else>
                  <LightCurve :detections="detections" :non_detections="non_detections"/>
+                  <v-tooltip bottom>
+                      <template v-slot:activator="{ on }">
+                        <span v-on="on">
+                          <span class="font-weight-black">
+                            Raise Time:
+                          </span>
+                           {{(first_detection.mjd-last_non_detection.mjd).toFixed(3)}} Days
+                        </span>
+                      </template>
+                      <span>Time between the last non detection and the first detection </span>
+                    </v-tooltip>
+
+                 </div>
+                 <div v-else>
+                   No non detections before first detection
+                 </div>
                </div>
+             </v-flex>
+             <v-flex  v-if="!loading" xs12>
+               <h5>
+                 Discovery Stamps
+               </h5>
+             </v-flex>
+             <v-flex xs4>
+               <v-img :src="scienceStamp"></v-img>
+               Science
+             </v-flex>
+             <v-flex xs4>
+               <v-img :src="templateStamp"></v-img>
+               Template
+             </v-flex>
+             <v-flex xs4>
+               <v-img :src="differenceStamp"></v-img>
+               Difference
              </v-flex>
            </v-layout>
          </v-card-text>
@@ -80,6 +111,7 @@
 import LightCurve from "@/components/LightCurve"
 
 var ztf_url = process.env.VUE_APP_PSQL_API
+var base_url = "https://avro.alerce.online/get_stamp"
 
 export default{
   components:{
@@ -91,7 +123,9 @@ export default{
       detections: [],
       scatter: {
         showLoading: true
-      }
+      },
+      first_detection: null,
+      last_non_detection: null
     }
   },
   props: {
@@ -102,7 +136,19 @@ export default{
   computed:{
     loading(){
       return !((this.detections.length > 0) && (this.non_detections.length > 0))
-    }
+    },
+    alerts(){
+      return {detections: this.detections, non_detections: this.non_detections}
+    },
+    differenceStamp(){
+      return base_url+"?oid="+this.first_detection.oid+"&candid="+ this.first_detection.candid_str +"&type=difference&format=png"
+    },
+    templateStamp(){
+      return base_url+"?oid="+this.first_detection.oid+"&candid="+ this.first_detection.candid_str +"&type=template&format=png"
+    },
+    scienceStamp(){
+      return base_url+"?oid="+this.first_detection.oid+"&candid="+ this.first_detection.candid_str +"&type=science&format=png"
+    },
   },
   mounted: function(){
     var self = this;
@@ -116,6 +162,28 @@ export default{
 
   },
   watch:{
+    alerts: function(newVal){
+      if(newVal.detections.length > 0 && newVal.non_detections.length > 0){
+        var first_detection = {mjd:Infinity}
+        var last_non_detection = {mjd: 0}
+
+        newVal.detections.forEach(function(detection){
+          if(first_detection.mjd > detection.mjd){
+            first_detection = detection
+          }
+        })
+        newVal.non_detections.forEach(function(non_detection){
+          if( non_detection.mjd < first_detection.mjd && last_non_detection.mjd < non_detection.mjd){
+              last_non_detection = non_detection
+          }
+        })
+        if(last_non_detection.mjd == 0){
+          last_non_detection = null
+        }
+        this.first_detection = first_detection
+        this.last_non_detection = last_non_detection
+      }
+    },
     alerce: function(newVal){
       var self = this;
       self.detections = [];
