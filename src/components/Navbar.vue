1beaca2b8f4e1fcc2655bdd6444f3c399cae380d
reyesdejong <reyes.dejong@gmail.com>
2021-09-24 18:02:29 -0300

    feature(new_login@Navbar) new login modal component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
   <v-toolbar dark color="dark">
-    <v-toolbar-side-icon :to="{'path':'/'}">
+    <v-toolbar-side-icon :to="{ path: '/' }">
       <v-img :src="require('../assets/logos/footerAlerceWhite.png')"></v-img>
     </v-toolbar-side-icon>
-    <v-toolbar-title :to="{'path':'/'}" class="white--text">
+    <v-toolbar-title :to="{ path: '/' }" class="white--text">
       <span class="hidden-sm-and-down">
         ALeRCE
       </span>
@@ -14,30 +14,27 @@
 
     <!-- <v-spacer></v-spacer> -->
 
-
-    <v-toolbar-items  class="ml-3 hidden-sm-and-down">
+    <v-toolbar-items class="ml-3 hidden-sm-and-down">
       <v-btn
-          flat
-           v-for="(item, index) in items"
-           :href="item.link"
-           class="text-capitalize"
-           v-bind:key="index"
-         >{{ item.title }}</v-btn>
-       </v-toolbar-items>
+        flat
+        v-for="(item, index) in items"
+        :href="item.link"
+        class="text-capitalize"
+        v-bind:key="index"
+        >{{ item.title }}</v-btn
+      >
+    </v-toolbar-items>
 
-   <v-menu
-    full-width
-    :z-index="priority"
-    class="hidden-md-and-up">
-     <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
-     <v-list>
-       <div v-for="(item, index) in items" v-bind:key="index">
-         <v-list-tile :href="item.link" >
-             <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-         </v-list-tile>
-       </div>
-     </v-list>
-   </v-menu>
+    <v-menu full-width :z-index="priority" class="hidden-md-and-up">
+      <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+      <v-list>
+        <div v-for="(item, index) in items" v-bind:key="index">
+          <v-list-tile :href="item.link">
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile>
+        </div>
+      </v-list>
+    </v-menu>
 
     <v-spacer></v-spacer>
     <!-- Buttons -->
@@ -46,72 +43,99 @@
         FAQ
       </v-btn>
     </v-toolbar-items>
-    <v-btn v-if="!logged" class="ma-2" tile outline @click="authenticate()">
-      <v-icon left>account_circle</v-icon> Sign in
-    </v-btn>
+    <!-- <v-btn v-if="!logged" class="ma-2" tile outline @click="authenticate()">
+      <v-icon left>account_circle</v-icon> Login
+    </v-btn> -->
+    <div v-if="!logged">
+
+      <div class="text-center">
+        <v-dialog v-model="loginModal" width="600">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn class="ma-2" tile outline v-bind="attrs" v-on="on">
+              <v-icon left>account_circle</v-icon> Login
+            </v-btn>
+          </template>
+          <login
+            @registerClick="showLoginModal"
+          ></login>
+        </v-dialog>
+      </div>
+    </div>
     <v-menu v-else offset-y>
       <template v-slot:activator="{ on }">
         <v-btn icon v-on="on">
           <v-avatar size="36px" :color="randomColor">
-              <span class="white--text headline">{{userInitials}}</span>
+            <span class="white--text headline">{{ userInitials }}</span>
           </v-avatar>
         </v-btn>
       </template>
       <!-- Logged -->
-        <v-list subheader dense>
-          <v-subheader>{{ user.name.charAt(0).toUpperCase() +
-           user.name.slice(1)}} {{ user.last_name.charAt(0).toUpperCase() +
-            user.last_name.slice(1)}}</v-subheader>
-          <!--Register-->
-          <v-list-tile @click="logout()" avatar>
-            <v-list-tile-avatar>
-              <v-icon>power_settings_new</v-icon>
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title> Log out</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
+      <v-list subheader dense>
+        <v-subheader
+          >{{ user.name.charAt(0).toUpperCase() + user.name.slice(1) }}
+          {{
+            user.last_name.charAt(0).toUpperCase() + user.last_name.slice(1)
+          }}</v-subheader
+        >
+        <!--Register-->
+        <v-list-tile @click="logout()" avatar>
+          <v-list-tile-avatar>
+            <v-icon>power_settings_new</v-icon>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title> Log out</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-menu>
   </v-toolbar>
 </template>
 <script>
-import {authMixin} from '@/mixins/authMixin';
+import { authMixin } from '@/mixins/authMixin';
+import Login from './users/Login';
 
 export default {
   /* eslint-disable */
-  name: "navbar",
+  name: 'navbar',
   mixins: [authMixin],
+  components: {
+    Login,
+  },
   data: () => ({
-  priority: 30,
-  items: [
-    { title: 'ZTF Explorer' , link:"http://alerce.online"},
-    { title: 'About', link:"http://alerce.science" },
+    priority: 30,
+    items: [
+      { title: 'ZTF Explorer', link: 'http://alerce.online' },
+      { title: 'About', link: 'http://alerce.science' },
     ],
+    loginModal: false,
   }),
-  mounted(){
-    if(localStorage.getItem('vue-authenticate.vueauth_token')){
-      this.$store.dispatch('getUserInfo')
+  mounted() {
+    if (localStorage.getItem('vue-authenticate.vueauth_token')) {
+      this.$store.dispatch('getUserInfo');
     }
-
+  },
+  methods: {
+    showLoginModal() {
+      this.loginModal = !this.loginModal;
+    },
   },
   computed: {
-    user(){
+    user() {
       return this.$store.getters.getUser;
     },
-    logged(){
-      return this.$store.getters.getUser.name == null? false : true;
+    logged() {
+      return this.$store.getters.getUser.name == null ? false : true;
     },
-    randomColor(){
-      return '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+    randomColor() {
+      return '#' + ((Math.random() * 0xffffff) << 0).toString(16);
     },
-    userInitials(){
-      if (!this.user.name){
-          return
+    userInitials() {
+      if (!this.user.name) {
+        return;
       }
-      return this.user.name[0] + this.user.last_name[0]
+      return this.user.name[0] + this.user.last_name[0];
     },
-  }
-}
+  },
+};
 /* eslint-enable */
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
+  icons: {
+    iconfont: 'mdi', // default - only for display purposes
+  },
 })
